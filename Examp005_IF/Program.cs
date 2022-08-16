a299561c87b4c8fa0529e1c4a52f7f723b3270a1
Rick <mrfox7398@mail.ru>
2022-08-16 22:58:53 +0300

    Условие

---
+++ Examp005_IF/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввувшее имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Рита")
+{
+    Console.WriteLine("Ритуся, пойдем дам тебе фаршу!");
+}
+else
+{
+    Console.Write("Дарова, ");
+    Console.WriteLine(username);
+}
